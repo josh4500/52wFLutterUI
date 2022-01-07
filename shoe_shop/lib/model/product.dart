f65435b20856d9aab8fda2b89d087a69e9f173df
josh4500 <jibo.ajosh45@gmail.com>
2022-01-07 02:46:14 +0100

    Added models

---
+++ shoe_shop/lib/model/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String name;
+  final String description;
+  final String image;
+  final double price;
+  final String category;
+  final String id;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.category,
+    required this.id,
+  });
+}
