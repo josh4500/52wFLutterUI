d638f4b87e4b3271e8519d7a709e953971a5dbf4
josh4500 <jibo.ajosh45@gmail.com>
2022-01-07 02:47:38 +0100

    Update with change notifier

--- shoe_shop/lib/main.dart
+++ shoe_shop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shoe_shop/provider/cart_provider.dart';
 
 import 'ui/homepage/homepage.dart';
 
@@ -11,24 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins"),
-      home: const HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins"),
+        home: const HomePage(),
+      ),
     );
-  }
-}
-
-class MyTransitionDelegate extends TransitionDelegate {
-  @override
-  Iterable<RouteTransitionRecord> resolve(
-      {required List<RouteTransitionRecord> newPageRouteHistory,
-      required Map<RouteTransitionRecord?, RouteTransitionRecord>
-          locationToExitingPageRoute,
-      required Map<RouteTransitionRecord?, List<RouteTransitionRecord>>
-          pageRouteToPagelessRoutes}) {
-    // TODO: implement resolve
-    throw UnimplementedError();
   }
 }
